8b41d02dd5cba6b3fcaba9a8d95a1cf8cbdb67c5
Anna-Grigencha <grigencha10@gmail.com>
2022-07-23 13:32:28 +0300

    feat: add register in pop-up

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const menu = document.querySelector(".header-nav");
 const body = document.querySelector(".body");
 const darkBackgraund = document.querySelector(".dark-bg");
 const menuLinks = document.querySelectorAll(".header-link");
+const login = document.querySelector(".button-login");
+const popUp = document.querySelector(".pop-up");
+const register = document.querySelector(".register");
+const btnContinueWithFacebook = document.querySelector(
+  ".btn-continue-with-facebook"
+);
+const btnContinueWithGoogle = document.querySelector(
+  ".btn-continue-with-google"
+);
+const groupOR = document.querySelector(".group-or");
+const forgotYourPassword = document.querySelector(".forgot-your-password");
+const contentPopUp = document.querySelector(".content-pop-up");
+const titlePopUp = document.querySelector(".name-pop-up");
+const btnSignInUp = document.querySelector(".btn-sign-in");
+const havingAnAccount = document.querySelector(".having-an-account");
 
 (function () {
   burger.addEventListener("click", () => {
@@ -38,48 +53,59 @@ const menuLinks = document.querySelectorAll(".header-link");
   });
 })();
 
-const login = document.querySelector(".button-login");
-const popUp = document.querySelector(".pop-up");
-
 (function () {
   login.addEventListener("click", () => {
     popUp.classList.add("open-pop-up");
   });
 })();
 
-const register = document.querySelector(".register");
-const btnContinueWithFacebook = document.querySelector(
-  ".btn-continue-with-facebook"
-);
-const btnContinueWithGoogle = document.querySelector(
-  ".btn-continue-with-google"
-);
-const groupOR = document.querySelector(".group-or");
-const forgotYourPassword = document.querySelector(".forgot-your-password");
-const contentPopUp = document.querySelector(".content-pop-up");
-
 (function () {
   register.addEventListener("click", () => {
-    btnContinueWithFacebook.classList.add("open-register");
-  });
-  register.addEventListener("click", () => {
-    btnContinueWithGoogle.classList.add("open-register");
-  });
-  register.addEventListener("click", () => {
-    groupOR.classList.add("open-register");
-  });
+    btnContinueWithFacebook.classList.toggle("open-register");
+    btnContinueWithGoogle.classList.toggle("open-register");
+    groupOR.classList.toggle("open-register");
+    forgotYourPassword.classList.toggle("open-register");
+    contentPopUp.classList.toggle("open-register");
 
-  register.addEventListener("click", () => {
-    forgotYourPassword.classList.add("open-register");
-  });
-
-  register.addEventListener("click", () => {
-    contentPopUp.classList.add("open-register");
+    if (register.innerHTML === "Log in") {
+      titlePopUp.innerHTML = "Log in to your account";
+      havingAnAccount.innerHTML = "Don’t have an account?";
+      register.innerHTML = "Register";
+      btnSignInUp.value = "Sign In";
+      document.querySelector(".input-email").value = " ";
+      document.querySelector(".input-password").value = " ";
+    } else {
+      titlePopUp.innerHTML = "Create account";
+      havingAnAccount.innerHTML = "Already have an account?";
+      register.innerHTML = "Log in";
+      btnSignInUp.value = "Sign Up";
+      document.querySelector(".input-email").value = " ";
+      document.querySelector(".input-password").value = " ";
+    }
   });
 })();
 
+popUp.addEventListener("click", (event) => {
+  if (event.target.classList.contains("open-pop-up")) {
+    popUp.classList.remove("open-pop-up");
+    btnContinueWithFacebook.classList.remove("open-register");
+    btnContinueWithGoogle.classList.remove("open-register");
+    groupOR.classList.remove("open-register");
+    forgotYourPassword.classList.remove("open-register");
+    contentPopUp.classList.remove("open-register");
+    titlePopUp.innerHTML = "Log in to your account";
+    havingAnAccount.innerHTML = "Don’t have an account?";
+    register.innerHTML = "Register";
+    btnSignInUp.value = "Sign In";
+    document.querySelector(".input-email").value = " ";
+    document.querySelector(".input-password").value = " ";
+  }
+});
+
 (function () {
-  contentPopUp.addEventListener("click", () => {
-    btnContinueWithFacebook.classList.add("open-register");
+  btnSignInUp.addEventListener("click", () => {
+    let email = document.querySelector(".input-email").value;
+    let password = document.querySelector(".input-password").value;
+    alert(`${email} ${password}`);
   });
 })();
